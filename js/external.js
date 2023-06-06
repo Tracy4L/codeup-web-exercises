911779ea47d5727508d9d22c983de2be49029e8b
Tracy <trecharles.smith@gmail.com>
2023-06-06 11:28:55 -0500

    added

---
+++ js/external.js
@@ -0,0 +1,5 @@
+console.log( "Hello from external JavaScript")
+
+alert('welcome to my website')
+prompt('whats your favorite color')
+alert('thats mine too were twins!!!!')
---
+++ readme.md
@@ -0,0 +1,4 @@
+# Ent Web Exersise
+
+
+This is a repository of coding exercises for the Codeup front-end curriculum.
