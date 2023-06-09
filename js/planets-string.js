d3c17d6fc70226c29ac41fb47d071cf7cc197cb5
Tracy <trecharles.smith@gmail.com>
2023-06-09 10:44:53 -0500

    added
    git status
    
    git

---
+++ js/planets-string.js
@@ -0,0 +1,69 @@
+// // // function(){
+// // //     "use strict";
+// //
+//
+// // let Planets = planetsString.split(,)
+// //
+// //
+// // console.log(Planets);
+// //
+// // //
+// // //     /**
+// //      * TODO:
+// //      * Convert planetsString to an array, and save it in a variable named
+// //      * planetsArray.
+// //      * console.log planetsArray to check your work
+// //      */ console.log(planetsArray)
+// // const planetStrings["Mercury|Venus|Earth|Mars|Jupiter|Saturn|Uranus|Neptune" ]
+// //     // console.log(planetsArray);
+//
+//   var planetsString = "Mercury|Venus|Earth|Mars|Jupiter|Saturn|Uranus|Neptune";
+//     var planetsArray = planetsString.split('|')
+// console.log(planetsArray)
+//
+// //
+// //
+// // //     /**
+// // //      * TODO:
+// // //      * Create a string with <br> tags between each planet. console.log() your
+// // // // //      * results. Why might this be useful?
+//     let Planets = planetsArray.join('<br>');
+//
+//     console.log(Planets);
+//     console.log(Planets);
+//
+// // // // {/*    */}
+// // // {/*//      * BONUS:*/}
+// // // {/*//      * Create another string that would display your planets in an undordered*/}
+// // {/*//      * list. You will need an opening AND closing <ul> tags around the entire*/}
+// // {/*//      * string, and <li> tags around each planet.*/}
+// // // {/*//      */*/}
+// //
+// // {/*})();*/}
+// //
+//
+// // display planets and unordered list
+// // opening and closing <ul>
+// // and <li> tags around the planets
+// // let planets =" Mars,Earth,Uranus,Saturn"
+// // const planetsForMe = planets.
+// // console.log(planetsForMe.join(','));
+// //
+// let planetString = "mars, saturn,juptier, venus";
+// let planetsArray =  planetString.split()
+//
+// planetsArray.
+
+
+ const players ={
+    chicago: "MJ",
+    caviliers: "Lebron",
+    lakers: "Kobe"
+
+
+
+};
+
+console.log(players.chicago);
+console.log(players.caviliers);
+console.log(players.lakers);
