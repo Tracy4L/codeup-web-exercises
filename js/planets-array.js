91c1c78372f4649fa18c70b3ec66760341b99e1f
Tracy <trecharles.smith@gmail.com>
2023-06-14 08:39:07 -0500

    added

---
+++ js/planets-array.js
@@ -0,0 +1,22 @@
+// // function(){
+// //     "use strict";
+//
+
+// let Planets = planetsString.split(,)
+//
+//
+// console.log(Planets);
+//
+//
+//     /**
+//      * TODO:
+     * Convert planetsString to an array, and save it in a variable named
+     * planetsArray.
+     * console.log planetsArray to check your work
+     */ console.log(planetsArray)
+const planetStrings["Mercury|Venus|Earth|Mars|Jupiter|Saturn|Uranus|Neptune" ]
+    // console.log(planetsArray);
+
+  var planetsString = "Mercury|Venus|Earth|Mars|Jupiter|Saturn|Uranus|Neptune";
+    var planetsArray = planetsString.split('|')
+console.log(planetsArray)
